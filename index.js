f202cf551cedfd4501cd9ea343391d7b7cbcf610
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-21 21:20:23 +0100

    makes it work

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
 
 const readFile = require('./lib/read-file')
 const { send } = require('micro')
+const { parse } = require('url')
 
 module.exports = async (request, response) => {
-  const data = await readFile('lib/data/data.xml')
-  send(response, 200, data)
+  const query = parse(request.url, true).query
+  if (query.xsd) {
+    console.log(query)
+    const data = await readFile('lib/data/xsd.xml')
+    send(response, 200, data)
+  } else if (request.method === 'POST') {
+    const data = await readFile('lib/data/data.xml')
+    send(response, 200, data)
+  } else {
+    const data = await readFile('lib/data/wsdl.xml')
+    send(response, 200, data)
+  }
 }
