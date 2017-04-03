0f0d31763b7ee269085f794d3ed86aac97c620d2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-03 09:43:59 +0200

    Updates Node

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { parse } = require('url')
 module.exports = async (request, response) => {
   const query = parse(request.url, true).query
   if (query.xsd) {
-    console.log(query)
     const data = await readFile('lib/data/xsd.xml')
     send(response, 200, data)
   } else if (request.method === 'POST') {
